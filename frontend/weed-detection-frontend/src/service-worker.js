71232155b12e43493fad205a3497b734d4b8642a
gideonotieno <bruceotieno25@gmail.com>
2025-02-25 18:20:21 +0300

    global workbox

--- frontend/weed-detection-frontend/src/service-worker.js
+++ frontend/weed-detection-frontend/src/service-worker.js
@@ -1,1 +1,20 @@
-##
+/* global workbox */
+import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate } from "workbox-strategies";
+
+// Pre-cache files
+precacheAndRoute(self.__WB_MANIFEST || []);
+
+// Cache API responses
+registerRoute(
+  ({ request }) => request.destination === "image",
+  new StaleWhileRevalidate({
+    cacheName: "images",
+  })
+);
+
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
